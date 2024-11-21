8c8dc0cd1cd3b0215bd31b2207837d15e7188920
Seb <147296428+sebaan-tupaea@users.noreply.github.com>
2024-11-21 14:46:55 +1300

    update feeding schedule route

---
+++ server/routes/feedingScheduleRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import * as db from '../db/zoo.ts'
+
+const router = Router()
+
+router.get('/feeding-schedule', async (req, res) => {
+  try {
+    const feedingSchedule = await db.getFeedingSchedule() 
+
+    res.json({ feedingSchedule }) 
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Feeding time is over' })
+  }
+})
+
+export default router
